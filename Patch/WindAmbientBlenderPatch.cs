e07a4a9f21fc7700f24b9dc0de97ddcb42505001
Viktor Engelowski <v.engelowski@gmail.com>
2025-05-13 23:46:05 +0200

    Patched method_7 of WindAmbientBlender
    + method_7 determines the "end_volume" of an audio crossfade
    + implemented a simple BepInEx config with a slider from 0 - 2 that reruns that crossfade method (method_3 with a forced fade)Add project files.

---
+++ Patch/WindAmbientBlenderPatch.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using SPT.Reflection.Patching;
+using Audio.AmbientSubsystem;
+using UnityEngine;
+
+
+namespace SPT_QuietWeather.Patch
+{
+    public class WindAmbientBlenderPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(WindAmbientBlender).GetMethod(nameof(WindAmbientBlender.method_7));
+
+        [PatchPrefix]
+        static bool Prefix(WindAmbientBlender __instance, ref float __result)
+        {
+            float clamped = Mathf.Clamp(__instance.method_9(), 0.2f, 1f);
+            __result = clamped * QuietWeather.WindVolumeMultiplier.Value;
+
+            return false; // skip original method
+        }
+    }
+}
