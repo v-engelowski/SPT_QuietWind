6ee917ace472906f1ee13bdd9001792559fe3d64
Viktor Engelowski <v.engelowski@gmail.com>
2025-05-13 23:46:05 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,39 @@
+﻿using Audio.AmbientSubsystem;
+using BepInEx;
+using BepInEx.Configuration;
+using SPT_QuietWeather.Patch;
+using System;
+
+
+namespace SPT_QuietWeather
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+    public class QuietWeather : BaseUnityPlugin
+    {
+        public static ConfigEntry<float> WindVolumeMultiplier;
+
+        private void Awake()
+        {
+            WindVolumeMultiplier = Config.Bind("Audio", "Wind volume multiplier", 1.0f, new ConfigDescription("Scales wind ambient volume.", new AcceptableValueRange<float>(0f, 2f)));
+            WindVolumeMultiplier.SettingChanged += OnWindVolumeChanged;
+
+            new WindAmbientBlenderPatch().Enable();
+            Logger.LogInfo($"[{PluginInfo.GUID} v{PluginInfo.VERSION}] Loaded plugin!");
+        }
+
+        private void OnWindVolumeChanged(object sender, EventArgs e)
+        {
+            var blender = FindObjectOfType<WindAmbientBlender>();
+
+            if (blender != null)
+            {
+                blender.method_3();
+                Logger.LogInfo($"[{PluginInfo.GUID} v{PluginInfo.VERSION}] Wind volume set!");
+            }
+            else
+            {
+                Logger.LogInfo($"[{PluginInfo.GUID} v{PluginInfo.VERSION}] Wind volume not set, no WindAmbientBlender (are you in a raid?)");
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPT_QuietWeather
+{
+    internal static class PluginInfo
+    {
+        public const String GUID = "com.makermacher.quietweather";
+        public const String NAME = "QuietWeather";
+        public const String VERSION = "0.0.1";
+    }
+}
